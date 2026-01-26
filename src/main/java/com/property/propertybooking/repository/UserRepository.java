fffc75d3e82618df7d704f105975e01a9c26a217
prasadlanjile1499 <prasadlanjile1499@gmail.com>
2026-01-26 18:16:22 +0530

    user CRUD Operations

--- src/main/java/com/property/propertybooking/repository/UserRepository.java
+++ src/main/java/com/property/propertybooking/repository/UserRepository.java
@@ -1,10 +1,27 @@
 package com.property.propertybooking.repository;
 
+import com.property.propertybooking.entity.Role;
 import com.property.propertybooking.entity.User;
+
+import org.springframework.boot.data.autoconfigure.web.DataWebProperties.Pageable;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
+    
+    
+    
+    
+
+    // Filter users by role (pagination)
+    Page<User> findByRole(Role role, PageRequest pageable);
+
+    
+    
+	// Search users by name (pagination)
+	Page<User> findByNameContainingIgnoreCase(String search, PageRequest pageable);
 }
--- src/main/java/com/property/propertybooking/service/UserService.java
+++ src/main/java/com/property/propertybooking/service/UserService.java
@@ -1,8 +1,14 @@
 package com.property.propertybooking.service;
 
 import com.property.propertybooking.dto.RegisterRequest;
+import com.property.propertybooking.dto.UpdateuserRequest;
+import com.property.propertybooking.entity.Role;
 import com.property.propertybooking.entity.User;
 import com.property.propertybooking.repository.UserRepository;
+
+import org.springframework.boot.data.autoconfigure.web.DataWebProperties.Pageable;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -33,5 +39,52 @@ public class UserService {
         user.setPassword(passwordEncoder.encode(request.getPassword()));
 
         return userRepository.save(user);
+    }
+    
+    
+ // 1 GET user by ID
+    public User getUserById(Long id) {
+        return userRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+    }
+    
+ //  UPDATE user
+    public User updateUser(Long id, UpdateuserRequest request) {
+        User user = getUserById(id);
+
+        user.setName(request.getName());
+        user.setPhone(request.getPhone());
+
+        if (request.getPassword() != null && !request.getPassword().isEmpty()) {
+            user.setPassword(passwordEncoder.encode(request.getPassword()));
+        }
+
+        return userRepository.save(user);
+    }
+
+    //  GET all users with pagination (ADMIN)
+    public Page<User> getAllUsers(
+            int page,
+            int size,
+            String search,
+            Role role) {
+
+        PageRequest pageable = PageRequest.of(page, size);
+
+        if (search != null && !search.isEmpty()) {
+            return userRepository.findByNameContainingIgnoreCase(search, pageable);
+        }
+
+        if (role != null) {
+            return userRepository.findByRole(role, pageable);
+        }
+
+        return userRepository.findAll(pageable);
+    }
+
+    //  DELETE user (ADMIN)
+    public void deleteUser(Long id) {
+        User user = getUserById(id);
+        userRepository.delete(user);
     }
 }
