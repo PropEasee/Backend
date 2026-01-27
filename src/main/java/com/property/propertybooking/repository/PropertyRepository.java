55348a0ff8f60f9a5a013088ca5760f4561a1568
prasadlanjile1499 <prasadlanjile1499@gmail.com>
2026-01-27 23:40:30 +0530

    Admin CRUD Operations

--- src/main/java/com/property/propertybooking/repository/PropertyRepository.java
+++ src/main/java/com/property/propertybooking/repository/PropertyRepository.java
@@ -1,9 +1,20 @@
 package com.property.propertybooking.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.property.propertybooking.entity.Property;
+import com.property.propertybooking.entity.PropertyStatus;
 
 public interface PropertyRepository extends JpaRepository<Property, Long>{
 	
+// Count properties by status (APPROVED / PENDING / REJECTED)
+    // Used in Admin Dashboard stats
+    long countByStatus(PropertyStatus status);
+
+    // Search properties by title (Admin can search all properties)
+    List<Property> findByTitleContainingIgnoreCase(String title);
+
+
 }
--- src/main/java/com/property/propertybooking/repository/UserRepository.java
+++ src/main/java/com/property/propertybooking/repository/UserRepository.java
@@ -1,14 +1,13 @@
 package com.property.propertybooking.repository;
 
-import com.property.propertybooking.entity.Role;
-import com.property.propertybooking.entity.User;
+import java.util.Optional;
 
-import org.springframework.boot.data.autoconfigure.web.DataWebProperties.Pageable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
+import com.property.propertybooking.entity.Role;
+import com.property.propertybooking.entity.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
@@ -24,4 +23,11 @@ public interface UserRepository extends JpaRepository<User, Long> {
     
 	// Search users by name (pagination)
 	Page<User> findByNameContainingIgnoreCase(String search, PageRequest pageable);
+
+
+// Count users by role (ADMIN / OWNER / USER)
+    // Used in Admin Dashboard stats
+    long countByRole(Role role);
+
+
 }
